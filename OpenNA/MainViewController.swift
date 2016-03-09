cdf0d0e7d6ccf1e31dd9e6415e02cd63cf0e9bc8
woogii <jhsw0201@naver.com>
2016-03-09 22:33:15 +0900

    feat : Implement function associated with segmentControl

--- OpenNA/MainViewController.swift
+++ OpenNA/MainViewController.swift
@@ -45,55 +45,58 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return lawmakers.count
+        
+        var count = 0
+        
+        switch segmentedControl.selectedSegmentIndex {
+            
+            case 0 :
+                count = lawmakers.count
+            case 1 :
+                count = 0
+                break
+            case 2 :
+                count = 0
+                break
+            default:
+                break
+        }
+        
+        return count
     }
+    
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
         
-        cell.textLabel!.text = lawmakers[indexPath.row].name
-        cell.detailTextLabel!.text = lawmakers[indexPath.row].party
+        
+        
+        switch segmentedControl.selectedSegmentIndex {
+            
+            case 0:
+                cell.textLabel!.text = lawmakers[indexPath.row].name
+                cell.detailTextLabel!.text = lawmakers[indexPath.row].party
+                break
+            
+            case 1:
+                break
+            
+            case 2:
+                break
+            
+            default:
+                break
+        }
+        
+
         
         
         return cell
     }
     
-    func loadData() {
-        
-      
-        
-//        guard let rawAJSONData = NSData(contentsOfFile:pathForJSONData) else {
-//            print("Can not get a raw JSON data in \(pathForJSONData)")
-//            return
-//        }
-//        
-//        let parsedResult:[[String:AnyObject]]!
-//        do {
-//            parsedResult = try NSJSONSerialization.JSONObjectWithData(rawAJSONData, options: .AllowFragments) as! [[String:AnyObject]]
-//            
-//            print(parsedResult.count)
-//            
-//            for dict in parsedResult {
-//                //print(dict)
-//                guard let name = dict["name_kr"] as? String else {
-//                    print("test")
-//                    return
-//                }
-//                lawmakers.append(name)
-//            }
-//            
-//        } catch let error as NSError {
-//            print(error.localizedDescription)
-//        }
-//        
-//        tableView.reloadData()
-        
-    }
-    
     @IBAction func segmentedControlChanged(sender: AnyObject) {
-        
+        tableView.reloadData()
     }
-    
-    
+
 }
 
