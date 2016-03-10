9f2dd10eb9158985f1723e831a700e6c13d36229
woogii <jhsw0201@naver.com>
2016-03-10 18:51:54 +0900

    feat : Implement TaskCancelingTableViewCell  that inherits from UITableViewCell for canceling url session task

---
+++ OpenNA/TaskCancelingTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TaskCancelingTableViewCell.swift
+//  OpenNA
+//
+//  Created by Hyun on 2016. 3. 10..
+//  Copyright © 2016년 wook2. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TaskCancelingTableViewCell : UITableViewCell {
+    
+
+    var taskToCancelifCellIsReused : NSURLSessionTask? {
+        
+        didSet {
+            
+            if let oldtask = oldValue {
+                oldtask.cancel()
+            }
+            
+        }
+    }
+    
+}
+
+
